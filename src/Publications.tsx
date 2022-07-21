814928777a4b3ccc2583bf576ef81d2c1bf781a0
Mahmud Azam <mahmud.azam@usask.ca>
2022-07-21 02:21:53 -0400

    Install and run react-latex

--- src/Publications.tsx
+++ src/Publications.tsx
@@ -1,8 +1,13 @@
 
+var Latex = require('react-latex');
+
 function Publications() {
   return (
   <div>
   Publications appear here
+  <div>
+  <Latex>$\infty$--operads are great</Latex>
+  </div>
   </div>
   );
 }
