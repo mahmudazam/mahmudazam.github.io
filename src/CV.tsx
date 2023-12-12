af89700fc43c1830e6966747108d029932627bd7
Mahmud Azam <mahmud.azam@usask.ca>
2023-12-11 23:34:19 -0600

    Improve CV style

--- src/CV.tsx
+++ src/CV.tsx
@@ -1,6 +1,8 @@
 
 import { PageDiv } from './App';
 
+import styled from 'styled-components';
+
 var Latex = require('react-latex');
 
 const education = [
@@ -52,22 +54,36 @@ function monthName(monthNum : number) {
   return date.toLocaleString('en-US', { month: 'long' })
 }
 
+const LayoutSpan = styled.span<{ small: string, align?: string }>`
+  @media only screen and (min-width: 768px) {
+    display: inline-block;
+    width: ${props => props.small};
+    text-align: ${props => props.align};
+  }
+  @media only screen and (max-width: 768px) {
+    display: block;
+    width:100%;
+  }
+`;
+
 function CV() {
   return (
   <PageDiv>
   <h3>Education</h3>
   {education.map((ed) =>
-    <div key={ed.degree} style={{marginTop: '5pt'}}>
-      <div>
-      <span style={{fontWeight: 'bold'}}>{ed.degree}</span>
-      {', '}
-      {monthName(ed.start.month)}, {ed.start.year}
-      {' --- '}
-      {ed.completed
-        ? `${monthName(ed.end.month)}, ${ed.end.year}`
-        : ' present'}
-      </div>
-      <span style={{fontStyle: 'italic'}}>{ed.institution}</span>
+    <div key={ed.degree} style={{ marginTop: '11pt' }}>
+      <LayoutSpan small='80%' style={{ fontWeight: 'bold' }}>
+        {ed.degree}
+      </LayoutSpan>
+      <LayoutSpan small='20%' align='end'>
+        {monthName(ed.start.month)}, {ed.start.year}
+        {' --- '}
+        {ed.completed
+          ? `${monthName(ed.end.month)}, ${ed.end.year}`
+          : ' present'}
+      </LayoutSpan>
+      <hr style={{ marginTop: '0%' }} />
+      <div style={{fontStyle: 'italic'}}>{ed.institution}</div>
       <div>Supervisor: {ed.supervisor}</div>
       <div>Thesis: {ed.thesis}</div>
     </div>
@@ -76,14 +92,17 @@ function CV() {
   <h3>Invited Talks</h3>
   {invitedTalks.map((talk) =>
     <div key={talk.title}>
-      <div>
-        <span style={{ fontWeight: 'bold' }}>{talk.title}</span>
-        {', '}
+      <LayoutSpan small='80%' style={{ fontWeight: 'bold' }}>
+        {talk.title}
+      </LayoutSpan>
+      <LayoutSpan small='20%' align='end'>
         {monthName(talk.date.month)} {talk.date.day}, {talk.date.year}
+      </LayoutSpan>
+      <hr style={{ marginTop: '0%' }} />
+      <div style={{ fontStyle: 'italic' }}>
+        {talk.event}
       </div>
       <div>
-        <span style={{ fontStyle: 'italic' }}>{talk.event}</span>
-        {', '}
         {talk.location}
       </div>
     </div>
