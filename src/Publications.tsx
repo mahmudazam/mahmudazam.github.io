40751e13e87f99b3e075fa63a5657aad006e058d
Mahmud Azam <mahmud.azam@usask.ca>
2023-12-12 19:55:02 -0600

    Refactor Publications to use Algebraic Types

--- src/Publications.tsx
+++ src/Publications.tsx
@@ -3,23 +3,55 @@ import { PageDiv } from './App';
 
 //var Latex = require('react-latex');
 
-const publications = [
+enum WhereTag {
+  JOURNAL = "Journal",
+  INREVIEW = "InReview",
+}
+
+interface Journal {
+  readonly tag: WhereTag.JOURNAL,
+  name: string,
+  volume: number,
+  number: number,
+  pages: { from : number, to: number },
+}
+
+interface InReview {
+  readonly tag: WhereTag.INREVIEW,
+  preprintURL?: { path : string, display: string },
+}
+
+interface Publication {
+  title: string,
+  author: Array<string>,
+  where: Journal | InReview,
+  time : { year : number, month : number, day: number },
+}
+
+const publications : Array<Publication> = [
 {
   title: 'TQFTs and Quantum Computing',
   author: ['Mahmud Azam', 'Steven Rayan'],
-  journal: 'Preprint',
-  url: {
-    path: 'https://arxiv.org/abs/2210.03556',
-    display: 'arXiv:2210.03556',
+  where: {
+    tag: WhereTag.INREVIEW,
+    preprintURL : {
+      path: 'https://arxiv.org/abs/2210.03556',
+      display: 'arXiv:2210.03556',
+    }
   },
   time: { year: 2022, month: 8, day: 15 },
 },
 {
   title: 'Uniformly quasi-Hermitian groups are supramenable',
   author: ['Mahmud Azam', 'Ebrahim Samei'],
-  journal: 'Canadian Mathematical Bulletin',
+  where: {
+    tag: WhereTag.JOURNAL,
+    name: 'Canadian Mathematical Bulletin',
+    volume: 65,
+    number: 3, 
+    pages: { from: 665, to: 673 }
+  },
   time: { year: 2021, month: 7, day: 14 },
-  pages: { from: 1, to: 9 }
 },
 ]
 
@@ -34,8 +66,28 @@ function Publications() {
       )}
     . {pub.time.year}
     . <span style={{fontStyle: 'italic'}}>{pub.title}</span>
-    . {pub.journal}{pub.pages && (', ' + pub.pages.from + '--' + pub.pages.to)}
-    . {pub.url && <a href={pub.url.path}>{pub.url.display}</a>}
+    . {(() => {
+      switch(pub.where.tag) {
+      case WhereTag.JOURNAL:
+        return `
+          ${pub.where.name}, ${pub.where.volume}(${pub.where.number}):
+          ${pub.where.pages.from} -- ${pub.where.pages.to}.
+        `;
+      case WhereTag.INREVIEW:
+        return (
+          <span>
+            In review
+            {pub.where.preprintURL && (
+              <span>
+                ; preprint: <a href={pub.where.preprintURL.path}>
+                  {pub.where.preprintURL.display}
+                </a>
+              </span>
+            )}.
+          </span>
+        );
+      } 
+      })()}
     </div>
   )}
   </PageDiv>
