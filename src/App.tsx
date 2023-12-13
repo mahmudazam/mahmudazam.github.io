f64d0885ddc60f5709aa94d8ea3a963341c3d2e1
Mahmud Azam <mahmud.azam@usask.ca>
2023-12-12 23:13:22 -0600

    Fix padding and add animation for PageDiv

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import Publications from './Publications';
 import { appRoutes } from './structure';
 
 import { Routes, Route } from 'react-router-dom';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 const AppDiv = styled.div`
   display: inline-block;
@@ -20,18 +20,30 @@ const AppDiv = styled.div`
   font-family: "Palatino", serif;
 `;
 
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`;
+
 export const PageDiv = styled.div`
   @media only screen and (min-width: 768px) {
     padding-left: 20%;
     padding-right: 20%;
-    padding-top: 5%;
+    padding-top: 11%;
+    padding-bottom: 7.5%;
   }
   @media only screen and (max-width: 768px) {
     padding-left: 7.5%;
     padding-right: 7.5%;
-    padding-top: 25%;
+    padding-top: 35%;
+    padding-bottom: 11%;
   }
   margin: '0%';
+  animation: ${fadeIn} 0.5s ease-in-out;
 `;
 
 const appElements = new Map<string, any>([
