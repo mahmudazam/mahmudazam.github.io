39428a2ac43909587a603d274a2e4263ca47bb21
Mahmud Azam <mahmud.azam@usask.ca>
2023-12-12 22:04:16 -0600

    Populate Awards list
    --- the information might need further checking

--- src/CV.tsx
+++ src/CV.tsx
@@ -41,11 +41,179 @@ const education = [
 const invitedTalks = [
 {
   title: 'TQFTs and Quantum Computing',
+  event: 'quanTA Junion Symposium',
+  date: { year: 2023, month: 6, day: 29 },
+  location: 'University of Saskatchewan'
+},
+{
+  title: 'TQFTs and Quantum Computing',
   event: 'Scientific Session on Quantum Information Theory, CMS Summer Meeting',
   date: { year: 2023, month: 6, day: 3 },
   location: 'University of Ottawa'
-}
+},
+{
+  title: 'TQFTs through Parallel Transport and Quantum Computing',
+  event: 'The Calgary Peripatetic Seminar',
+  date: { year: 2021, month: 9, day: 24 },
+  location: 'University of Calgary'
+},
 ];
+
+const awards = [
+{
+  name: 'Canada Graduate Scholarship --- Doctoral',
+  authority: 'NSERC',
+  institution: 'University of Saskatchewan',
+  currency: 'CA$',
+  value: '105,000 for 3 years',
+  period: {
+    start: { year: 2023, month: 9, day: 1 },
+    end: { year: 2026, month: 8, day: 31 }
+  }
+},
+{
+  name: 'Dean\'s Scholarship',
+  institution: 'University of Saskatchewan',
+  currency: 'CA$',
+  value: '66,000 for 3 years',
+  period: {
+    start: { year: 2022, month: 9, day: 1 },
+    end: { year: 2023, month: 8, day: 31 }
+  },
+  endNote: 'switched to NSERC CGS D after first year'
+},
+{
+  name: 'Ontario Graduate Scholarship',
+  authority: 'Government of Ontario',
+  institution: 'University of Toronto',
+  currency: 'CA$',
+  value: '15,000',
+  period: {
+    start: { year: 2021, month: 9, day: 1 },
+    end: { year: 2022, month: 8, day: 31 }
+  }
+},
+{
+  name: 'George FD Duff Graduate Fellowship in Mathematics',
+  institution: 'University of Toronto',
+  currency: 'CA$',
+  value: '1,500',
+  period: {
+    term: 'Fall',
+    year: 2021
+  }
+},
+{
+  name: 'Undergraduate Student Research Award',
+  authority: 'NSERC',
+  institution: 'University of Saskatchewan',
+  currency: 'CA$',
+  value: '6,000',
+  period: {
+    start: { year: 2021, month: 5, day: 1 },
+    end: { year: 2021, month: 8, day: 31 }
+  }
+},
+{
+  name: 'Arts & Science Undergraduate Student Award',
+  institution: 'University of Saskatchewan',
+  currency: 'CA$',
+  value: '1,000',
+  period: {
+    description: 'Academic year 2020 --- 2021'
+  }
+},
+{
+  name: 'Most Outstanding Graduate in Mathematics',
+  institution: 'University of Saskatchewan',
+  period: {
+    description: 2021
+  }
+},
+{
+  name: 'Best Thesis Prize in Mathematics',
+  institution: 'University of Saskatchewan',
+  period: {
+    description: 2021
+  }
+},
+{
+  name: 'Haslam Medal',
+  description: 'for most outstanding graduate in College of Arts and Science',
+  institution: 'University of Saskatchewan',
+  period: {
+    description: 2021
+  }
+},
+{
+  name: 'Earl of Bessborough Prize',
+  description: 'for most outstanding graduate in science',
+  institution: 'University of Saskatchewan',
+  period: {
+    description: 2021
+  }
+},
+{
+  name: 'Undergraduate Student Research Award',
+  authority: 'NSERC',
+  institution: 'University of Saskatchewan',
+  currency: 'CA$',
+  value: '4,500',
+  period: {
+    start: { year: 2020, month: 5, day: 1 },
+    end: { year: 2020, month: 8, day: 31 }
+  }
+},
+{
+  name: 'JP Tremblay Award',
+  authority: 'Department of Computer Science',
+  institution: 'University of Saskatchewan',
+  period: {
+    description: 'Academic years 2018 --- 2019, 2016 --- 2017'
+  }
+},
+{
+  name: 'JS & JSG Middleton Scholarship',
+  institution: 'University of Saskatchewan',
+  currency: 'CA$',
+  value: '3,000',
+  period: {
+    start: { year: 2019, month: 8, day: 31 },
+    end: { year: 2019, month: 12, day: 31 }
+  }
+},
+{
+  name: 'University of Saskatchewan Scholarship',
+  institution: 'University of Saskatchewan',
+  currency: 'CA$',
+  value: '3,000',
+  period: {
+    start: { year: 2017, month: 12, day: 31 },
+    end: { year: 2018, month: 4, day: 31 }
+  }
+},
+{
+  name: 'Shell Canada Scholarship',
+  institution: 'University of Saskatchewan',
+  currency: 'CA$',
+  value: '2,750',
+  period: {
+    start: { year: 2017, month: 9, day: 1 },
+    end: { year: 2017, month: 12, day: 31 }
+  }
+},
+{
+  name: 'Undergraduate Student Research Award',
+  authority: 'NSERC',
+  institution: 'University of Saskatchewan',
+  currency: 'CA$',
+  value: '4,500',
+  period: {
+    start: { year: 2017, month: 5, day: 1 },
+    end: { year: 2017, month: 8, day: 31 }
+  }
+},
+]
 
 function monthName(monthNum : number) {
   const date = new Date();
@@ -73,17 +241,17 @@ function CV() {
   <hr style={{ marginTop: '0%' }} />
   {education.map((ed) =>
     <div key={ed.degree} style={{ marginTop: '0.5em' }}>
-      <LayoutSpan small='80%' style={{ fontWeight: 'bold' }}>
-        {ed.degree}
+      <LayoutSpan small='70%'>
+        <span style={{ fontWeight: 'bold' }}>{ed.degree}</span>
+        , <span style={{fontStyle: 'italic'}}>{ed.institution}</span>
       </LayoutSpan>
-      <LayoutSpan small='20%' align='end'>
+      <LayoutSpan small='30%' align='end'>
         {monthName(ed.start.month)}, {ed.start.year}
         {' --- '}
         {ed.completed
           ? `${monthName(ed.end.month)}, ${ed.end.year}`
           : ' present'}
-      </LayoutSpan>
-      <div style={{fontStyle: 'italic'}}>{ed.institution}</div>
+      </LayoutSpan> 
       <div>Supervisor: {ed.supervisor}</div>
       <div>Thesis: {ed.thesis}</div>
     </div>
@@ -93,17 +261,53 @@ function CV() {
   <hr style={{ marginTop: '0%' }} />
   {invitedTalks.map((talk) =>
     <div key={talk.title} style={{ marginTop: '0.5em' }}>
-      <LayoutSpan small='80%' style={{ fontWeight: 'bold' }}>
+      <LayoutSpan small='70%' style={{ fontWeight: 'bold' }}>
         {talk.title}
       </LayoutSpan>
-      <LayoutSpan small='20%' align='end'>
+      <LayoutSpan small='30%' align='end'>
         {monthName(talk.date.month)} {talk.date.day}, {talk.date.year}
       </LayoutSpan>
-      <div style={{ fontStyle: 'italic' }}>
-        {talk.event}
-      </div>
       <div>
-        {talk.location}
+        <span style={{ fontStyle: 'italic' }}>{talk.event}</span>
+        , {talk.location}
+      </div>
+    </div>
+  )}
+
+  <div style={{ fontWeight: 'bold', marginTop: '1.5em' }}>Awards</div>
+  <hr style={{ marginTop: '0%' }} />
+  {awards.map((award) =>
+    <div key={award.name} style={{ marginTop: '0.5em' }}>
+      <LayoutSpan small='70%'>
+        <span style={{ fontWeight: 'bold' }}>{award.name}</span>
+        {award.currency && award.value &&
+          <span> ({award.currency} {award.value})</span>}
+        {award.description && <span> ({award.description})</span>}
+      </LayoutSpan>
+      <LayoutSpan small='30%' align='end'>
+        {award.period.start &&
+          monthName(award.period.start.month) + ', ' + award.period.start.year}
+        {award.period.end &&
+          ' --- ' + monthName(award.period.end.month) +
+          ', ' + award.period.end.year}
+        {award.period.term && award.period.year &&
+          award.period.term + ', ' + award.period.year}
+        {award.period.description && award.period.description}
+      </LayoutSpan>
+      <div>
+        {award.authority &&
+        <span>
+            Awarded by <span style={{ fontStyle: 'italic' }}>
+              {award.authority}
+            </span> {' at '}
+        </span>}
+        <span style={{ fontStyle: 'italic' }}>
+          {award.institution}
+        </span>
+        {award.endNote &&
+        <span>
+          {', ' + award.endNote}
+        </span>}
       </div>
     </div>
   )}
